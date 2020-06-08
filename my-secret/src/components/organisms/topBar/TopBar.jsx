776e342712bcd60f69f108efcc507c1ad16c3d7b
NowApoca <manueldiez123@gmail.com>
2020-06-08 01:36:30 -0300

    img button

--- my-secret/src/components/organisms/topBar/TopBar.jsx
+++ my-secret/src/components/organisms/topBar/TopBar.jsx
@@ -7,13 +7,13 @@ export default function TopBar(props){
     return (
         <div className='topBar'>
             <div className='topbar-flex-box settings'>
-                <ImgButton />
+                <ImgButton img={'./logo512.png'} />
             </div>
             <div className='topbar-flex-box homeButton'>
                 <Button color='blue' pressed={true} fa='rocket' text='HOME' />
-                <Button color='blue' text='HOME'/>
-                <Button color='blue' text='HOME'/>
-                <Button color='blue' text='HOME'/>
+                <Button color='blue' text='FRIENDS'/>
+                <Button color='blue' text='ALL'/>
+                <Button color='blue' text='DECRIPTED'/>
             </div>
             <div className='topbar-flex-box logout'>
                 <Button color='red' fa='circle' text='LOGOUT' />
