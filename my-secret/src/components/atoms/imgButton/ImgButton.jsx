226aec4dfa15eddbc570474098a8ee9ad3992eab
NowApoca <diez.manuel98@gmail.com>
2020-06-09 01:25:33 -0300

    combining flexbox with grid

--- my-secret/src/components/atoms/imgButton/ImgButton.jsx
+++ my-secret/src/components/atoms/imgButton/ImgButton.jsx
@@ -1,7 +1,7 @@
 import React, { useState} from 'react';
+import './imgButton.css'
 
 export default function ImgButton(props){
-    console.log(props)
     return (
         <div >
             <img className='circular' src={require('../../../images' + props.src)} />  
--- my-secret/src/components/organisms/homeContent/HomeContent.jsx
+++ my-secret/src/components/organisms/homeContent/HomeContent.jsx
@@ -1,10 +1,60 @@
 import React from 'react';
+import ImgButton from '../../atoms/imgButton/ImgButton'
 import './homeContent.css'
 
 export default function HomeContent(props){
+    const publications = [{
+        text: "5",
+        encryptedText: "5",
+        user: "userTest",
+        timestamp: new Date(),
+        userImage: "noPhoto.png"
+    },{
+        text: "5",
+        encryptedText: "5",
+        user: "userTest",
+        timestamp: new Date(),
+        userImage: "noPhoto.png"
+    }]
+    const rows = [];
+    publications.map( publication => {
+        rows.push(
+            <div className='homeGrid2'>
+                <div className='white1'>
+                    hola 5
+                </div>
+                <div className='white2'>
+                    hola 6
+                </div>
+                <div className='topBarPublication'>
+                    <div className='prueba1'>
+                        <div className='prueba2'>
+                            <h6>hola1</h6>
+                            <h6>hola2</h6>
+                        </div>
+                    </div>
+                </div>
+                <div className='normalText'>
+                    Normal text
+                </div>
+                <div className='encryptedText'>
+                    Encripted text
+                </div>
+                <div className='decrypted'>
+                    decripted
+                </div>
+                <div className='publicationFooter'>
+                    Footer
+                </div>
+                <div className='comments'>
+                    Comments
+                </div>
+            </div>
+        )
+    })
     return (
         <div className='home'>
-        SOY UNA HOME CONTENT
+            {rows}
         </div>
     )
 }
